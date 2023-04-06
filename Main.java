32c575db611ee1106329acf849df23ca67dfe6fc
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-04-06 08:33:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] number = {50, 60, 10, 38, 65, 87, 56, 90, 100, 66, 80, 88, 40, 98};
+        for (int n : number) {
+            if (n > 50) {
+                System.out.println("number" + n);
+            }
+        }
+    }
+}
